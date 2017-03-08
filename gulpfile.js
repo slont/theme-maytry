7081e700049bc114487c712cc85588268076abad
Renma Yamazaki <renma.yamazaki@syncthought.com>
2017-03-08 17:59:24 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const cleanCss = require('gulp-clean-css')
+const rename = require('gulp-rename')
+const plumber = require('gulp-plumber')
+
+gulp.task('js', function() {
+	gulp.src('src/js/*.js')
+		.pipe(plumber())
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js/'))
+});
+
+gulp.task('scss', function() {
+	gulp.src('src/scss/app.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(cleanCss())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('assets/css/'))
+})
+
+gulp.task('default', ['js', 'scss'])
